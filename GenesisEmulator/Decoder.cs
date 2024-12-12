b6d963cdf6e2eefb174314e32cc1c1df03264f6f
AveryMadness <averyfd@shaw.ca>
2024-12-11 20:21:54 -0700

    a lot more stuff, unfinished.

--- GenesisEmulator/Decoder.cs
+++ GenesisEmulator/Decoder.cs
@@ -1,10 +1,33 @@
-﻿namespace GenesisEmulator;
+﻿using System.Diagnostics;
+using System.Reflection;
+using GenesisEmulator.BaseRegister;
+using GenesisEmulator.Target;
+
+namespace GenesisEmulator;
 
 public class Decoder(MemoryManager memoryManager, uint Start)
 {
     private MemoryManager _memory = memoryManager;
     private uint _start = Start;
     private ushort _instructionWord = 0;
+
+    private const byte OPCG_BIT_OPS = 0x0;
+    private const byte OPCG_MOVE_BYTE = 0x1;
+    private const byte OPCG_MOVE_LONG = 0x2;
+    private const byte OPCG_MOVE_WORD = 0x3;
+    private const byte OPCG_MISC = 0x04;
+    private const byte OPCG_ADDQ_SUBQ = 0x5;
+    private const byte OPCG_BRANCH = 0x6;
+    private const byte OPCG_MOVEQ = 0x7;
+    private const byte OPCG_DIV_OR = 0x8;
+    private const byte OPCG_SUB = 0x9;
+    private const byte OPCG_ALINE = 0xA;
+    private const byte OPCG_CMP_EOR = 0xB;
+    private const byte OPCG_MUL_AND = 0xC;
+    private const byte OPCG_ADD = 0xD;
+    private const byte OPCG_SHIFT = 0xE;
+    private const byte OPCG_FLINE = 0xF;
+
     
     public Instruction Decode()
     {
@@ -13,7 +36,7 @@ public class Decoder(MemoryManager memoryManager, uint Start)
 
         switch ((byte)((ins & 0xF000) >> 12))
         {
-            case 0x0:
+            case OPCG_BIT_OPS:
             {
                 ushort optype = (ushort)((ins & 0x0F00) >> 8);
 
@@ -57,10 +80,37 @@ public class Decoder(MemoryManager memoryManager, uint Start)
                 }
                 else if (((byte)ins & 0x138) == 0x108)
                 {
-                    byte dreg = (byte)((ins & 0x0E00) >> 9);
-                    byte areg = (byte)(ins & 0x0007);
-                    
-                    
+                    byte dreg = GetHighReg(ins);
+                    byte areg = GetLowReg(ins);
+
+                    Direction dir = (ins & 0x0080) == 0 ? Direction.FromTarget : Direction.ToTarget;
+                    Size size = (ins & 0x0040) == 0 ? Size.Word : Size.Long;
+                    short offset = (short)ReadUInt16();
+
+                    return new MOVEP(dreg, areg, offset, size, dir);
+                }
+                else if ((ins & 0x0100) == 0x100 || (ins & 0x0F00) == 0x0800)
+                {
+                    Target.Target bitnum = (ins & 0x0100) == 0x0100
+                        ? new Target.DirectDReg(GetHighReg(ins))
+                        : new Target.Immediate(ReadUInt16());
+
+                    Target.Target target = DecodeLowerEffectiveAddress(ins, Size.Byte);
+                    Size size = Size.None;
+
+                    if (target is Target.DirectAReg || target is DirectDReg)
+                    {
+                        size = Size.Long;
+                    }
+                    else
+                    {
+                        size = Size.Byte;
+                    }
+
+                    switch ((ins & 0x00C0) >> 6)
+                    {
+                        case 0b00: return 
+                    }
                 }
             }
         }
@@ -72,5 +122,157 @@ public class Decoder(MemoryManager memoryManager, uint Start)
         ushort value = _memory.ReadUInt16(_start);
         _start += 2;
         return value;
+    }
+
+    public uint ReadUInt32()
+    {
+        uint value = _memory.ReadUInt32(_start);
+        _start += 4;
+        return value;
+    }
+
+    public byte GetHighReg(ushort ins)
+    {
+        return (byte)((ins & 0x0E00) >> 9);
+    }
+
+    public byte GetLowReg(ushort ins)
+    {
+        return (byte)(ins & 0x0007);
+    }
+
+    public byte GetHighMode(ushort ins)
+    {
+        return (byte)((ins & 0x01C0) >> 6);
+    }
+
+    public byte GetLowMode(ushort ins)
+    {
+        return (byte)((ins & 0x038) >> 3);
+    }
+
+    public Target.Target DecodeLowerEffectiveAddress(ushort ins, Size? size)
+    {
+        byte reg = GetLowReg(ins);
+        byte mode = GetLowMode(ins);
+        return GetModeAsTarget(mode, reg, size.Value);
+    }
+
+    public Target.Target DecodeUpperEffectiveAddress(ushort ins, Size? size)
+    {
+        byte reg = GetHighReg(ins);
+        byte mode = GetHighMode(ins);
+        return GetModeAsTarget(mode, reg, size.Value);
+    }
+
+    public Target.Target GetModeAsTarget(byte mode, byte reg, Size size)
+    {
+        switch (mode)
+        {
+            case 0b000: return new Target.DirectDReg(reg);
+            case 0b001: return new Target.DirectAReg(reg);
+            case 0b010: return new Target.IndirectAReg(reg);
+            case 0b011: return new Target.IndirectARegInc(reg);
+            case 0b100: return new Target.IndirectARegDec(reg);
+            case 0b101:
+            {
+                int displacement = SignExtendToLong((uint)ReadUInt16(), Size.Word);
+                return new Target.IndirectRegOffset(new AReg(reg), null, displacement);
+            }
+            case 0b110:
+            {
+                return DecodeExtensionWord(reg);
+            }
+            case 0b111:
+            {
+                switch (reg)
+                {
+                    case 0b000:
+                    {
+                        uint value = (uint)SignExtendToLong((uint)ReadUInt16(), Size.Word);
+                        return new Target.IndirectMemory(value, Size.Word);
+                    }
+                    case 0b001:
+                    {
+                        uint value = ReadUInt32();
+                        return new Target.IndirectMemory(value, Size.Long);
+                    }
+                    case 0b010:
+                    {
+                        int displacement = SignExtendToLong((uint)ReadUInt16(), Size.Word);
+                        return new Target.IndirectRegOffset(new PC(), null, displacement);
+                    }
+                    case 0b011:
+                    {
+                        return DecodeExtensionWord(null);
+                    }
+                    case 0b100:
+                    {
+                        uint data = 0;
+                        switch (size)
+                        {
+                            case Size.Byte:
+                            {
+                                data = (uint)ReadUInt16();
+                                break;
+                            }
+                            case Size.Word:
+                            {
+                                data = (uint)ReadUInt16();
+                                break;
+                            }
+                            case Size.Long:
+                            {
+                                data = ReadUInt32();
+                                break;
+                            }
+                            default:
+                                throw new Exception();
+                        }
+                        return new Target.Immediate(data);
+                    }
+                }
+                break;
+            }
+        }
+
+        throw new Exception();
+    }
+
+    public Target.Target DecodeExtensionWord(byte? areg)
+    {
+        ushort briefExtension = ReadUInt16();
+
+        bool useBrief = (briefExtension & 0x0100) == 0;
+
+        byte xregNum = (byte)((briefExtension & 0x7000) >> 12);
+
+        XRegister.XRegister xReg = (briefExtension & 0x8000) == 0
+            ? new XRegister.DReg(xregNum)
+            : new XRegister.AReg(xregNum);
+
+        Size size = (briefExtension & 0x0800) == 0 ? Size.Word : Size.Long;
+
+        IndexRegister indexReg = new IndexRegister(xReg, scale: 0, size);
+        int displacement = SignExtendToLong((uint)(briefExtension & 0x00FF), Size.Byte);
+
+        if (areg is not null)
+        {
+            return new Target.IndirectRegOffset(new BaseRegister.AReg(areg.Value), indexReg, displacement);
+        }
+        else
+        {
+            return new Target.IndirectRegOffset(new BaseRegister.PC(), indexReg, displacement);
+        }
+    }
+    
+    public int SignExtendToLong(uint value, Size size)
+    {
+        switch (size)
+        {
+            case Size.Byte: return (int)(sbyte)(byte)value;
+            case Size.Word: return (int)(Int16)(UInt16)value;
+            case Size.Long: return (int)value;
+        }
     }
 }
--- GenesisEmulator/Instruction.cs
+++ GenesisEmulator/Instruction.cs
@@ -1,72 +1,204 @@
 ﻿using System.Net.Mail;
 
-namespace GenesisEmulator;
-
-public class Instruction
+namespace GenesisEmulator
 {
-    public virtual bool Execute()
+    public enum Direction
     {
-        return false;
+        FromTarget,
+        ToTarget
     }
-}
 
-public class ORtoCCR(byte data) : Instruction
-{
-    private byte Data = data;
-
-    public override bool Execute()
+    public enum Size
     {
-        return false;
+        None,
+        Byte,
+        Word,
+        Long
     }
-}
 
-public class ANDtoCCR(byte data) : Instruction
-{
-    private byte Data = data;
-
-    public override bool Execute()
+    public enum Sign
     {
-        return false;
+        Signed,
+        Unsigned
     }
-}
 
-public class EORtoCCR(byte data) : Instruction
-{
-    private byte Data = data;
-
-    public override bool Execute()
+    namespace XRegister
     {
-        return false;
+        public abstract record XRegister;
+
+        public record DReg(byte Reg) : XRegister;
+
+        public record AReg(byte Reg) : XRegister;
     }
-}
 
-public class ORtoSR(ushort data) : Instruction
-{
-    private ushort Data = data;
-
-    public override bool Execute()
+    namespace BaseRegister
     {
-        return false;
+        public abstract record BaseRegister;
+
+        public record None : BaseRegister;
+
+        public record PC : BaseRegister;
+
+        public record AReg(byte Reg) : BaseRegister;
     }
-}
 
-public class ANDtoSR(ushort data) : Instruction
-{
-    private ushort Data = data;
-
-    public override bool Execute()
+    public struct IndexRegister(XRegister.XRegister reg, byte scale, Size size)
     {
-        return false;
+        public XRegister.XRegister XReg = reg;
+        public byte Scale = scale;
+        public Size Size = size;
     }
-}
 
-public class EORtoSR(ushort data) : Instruction
-{
-    private ushort Data = data;
-
-    public override bool Execute()
+    namespace RegOrImmediate
     {
-        return false;
-    }
-}
+        public abstract record RegOrImmediate;
 
+        public record DReg(byte Reg) : RegOrImmediate;
+
+        public record Immediate(byte Reg) : RegOrImmediate;
+    }
+
+    public enum ControlRegister
+    {
+        VBR
+    }
+
+    public enum Condition
+    {
+        True,
+        False,
+        High,
+        LowOrSame,
+        CarryClear,
+        CarrySet,
+        NotEqual,
+        Equal,
+        OverflowClear,
+        OverflowSet,
+        Plus,
+        Minus,
+        GreaterThanOrEqual,
+        LessThan,
+        GreaterThan,
+        LessThanOrEqual
+    }
+
+    namespace Target
+    {
+        public abstract record Target;
+
+        public record Immediate(uint Value) : Target;
+
+        public record DirectDReg(byte Register) : Target;
+
+        public record DirectAReg(byte Register) : Target;
+
+        public record IndirectAReg(byte Register) : Target;
+
+        public record IndirectARegInc(byte Register) : Target;
+
+        public record IndirectARegDec(byte Register) : Target;
+
+        public record IndirectRegOffset(BaseRegister.BaseRegister Base, IndexRegister? Index, int Offset) : Target;
+
+        public record IndirectMemoryPreindexed(
+            BaseRegister.BaseRegister Base,
+            IndexRegister? Index,
+            int Offset,
+            int Displacement)
+            : Target;
+
+        public record IndirectMemoryPostindexed(
+            BaseRegister.BaseRegister Base,
+            IndexRegister? Index,
+            int Offset,
+            int Displacement)
+            : Target;
+
+        public record IndirectMemory(uint Address, Size Size) : Target;
+    }
+
+    public class Instruction
+    {
+        public virtual bool Execute()
+        {
+            return false;
+        }
+    }
+
+    public class ORtoCCR(byte data) : Instruction
+    {
+        private byte Data = data;
+
+        public override bool Execute()
+        {
+            return false;
+        }
+    }
+
+    public class ANDtoCCR(byte data) : Instruction
+    {
+        private byte Data = data;
+
+        public override bool Execute()
+        {
+            return false;
+        }
+    }
+
+    public class EORtoCCR(byte data) : Instruction
+    {
+        private byte Data = data;
+
+        public override bool Execute()
+        {
+            return false;
+        }
+    }
+
+    public class ORtoSR(ushort data) : Instruction
+    {
+        private ushort Data = data;
+
+        public override bool Execute()
+        {
+            return false;
+        }
+    }
+
+    public class ANDtoSR(ushort data) : Instruction
+    {
+        private ushort Data = data;
+
+        public override bool Execute()
+        {
+            return false;
+        }
+    }
+
+    public class EORtoSR(ushort data) : Instruction
+    {
+        private ushort Data = data;
+
+        public override bool Execute()
+        {
+            return false;
+        }
+    }
+
+    public class MOVEP(byte dreg, byte areg, short offset, Size size, Direction direction) : Instruction
+    {
+        private byte DReg = dreg;
+        private byte AReg = areg;
+        private short Offset = offset;
+        private Size size = size;
+        private Direction direction = direction;
+
+        public override bool Execute()
+        {
+            return false;
+        }
+    }
+    
+    
+}
